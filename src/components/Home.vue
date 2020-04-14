ff4071036b40cee6ab56d6128b5abbe65ab40ffc
Lilkhachatryan <Lilkhachatyan>
2020-04-14 13:34:17 +0400

    custom directive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>Home component</div>
+  <div>
+    <div v-custom-on:click="handleClick">Home component</div>
+    <input v-focus>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeComponent'
+  name: 'HomeComponent',
+  directives: {
+    customOn: {
+      // eslint-disable-next-line no-unused-vars
+      bind (el, binding, vnode) {
+        const { arg:type, value:fn } = binding;
+        el.addEventListener(type, fn);
+      },
+      unbind(el, binding) {
+        const { arg:type, value:fn } = binding;
+        el.removeEventListener(type, fn);
+      }
+    },
+    focus: {
+      inserted (el) {
+        el.focus();
+      }
+    }
+  },
+  methods: {
+    handleClick (e) {
+      console.log('e', e);
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  div {
+    &:focus {
+      box-shadow: 3px 3px #F08080;
+    }
+  }
 </style>
